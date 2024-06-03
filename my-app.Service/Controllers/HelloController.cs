d95c75849810f516b0ddd6e4755347829cc52db4
Alvari1980 <101476395+Alvari1980@users.noreply.github.com>
2024-06-03 12:32:31 +0000

    new version

--- my-app.Service/Controllers/HelloController.cs
+++ my-app.Service/Controllers/HelloController.cs
@@ -41,5 +41,25 @@ namespace my_app.Service.Controllers
             return Ok(ex.Message);
            }
         }
+
+        [HttpPost]
+        [Route("externalpost")]
+        public async Task<IActionResult> ExternalPost()
+        {
+            const string url = "https://dotnet-app-user6-user6-application.apps.cluster-5shjw.dynamic.redhatworkshops.io/Hello/hello4";
+
+           try{
+ HttpResponseMessage response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            return Ok(response.Content.ReadAsStringAsync());
+
+           }
+           catch(Exception ex)
+           {
+            return Ok(ex.Message);
+           }
+        }
     }
 }
